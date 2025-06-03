17e92ea5fd9d3ec3a1fb35e42fe933e9c3f243d4
BrandonEastwell <brandoneastwell@yahoo.com>
2025-06-03 20:45:02 +0100

    Extracts metadata from image uploads

--- backend/photoapp/media/views.py
+++ backend/photoapp/media/views.py
@@ -1,8 +1,18 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from PIL import Image
+from PIL.ExifTags import TAGS
 
 def image_upload(req):
     if req.method != "POST":
         return JsonResponse( { "error": "Request method invalid." }, status=405 )
 
-    photo = req.FILES.get('image')
+    image_file = req.FILES.get('image')
+    image = Image.open(image_file)
+    exif_data = image.getexif()
+    image_tags = {}
+
+    for tag_id in exif_data:
+        tag_name = TAGS.get(tag_id, tag_id)
+        tag_value = exif_data.get(tag_id)
+        image_tags[tag_name] = tag_value
