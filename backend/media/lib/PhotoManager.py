570f92bcfb59d408c6176e7bce5307ab0ba97e38
BrandonEastwell <brandoneastwell@yahoo.com>
2025-06-13 18:15:08 +0100

    Fixed weighting calculation for exif sorting

--- backend/media/lib/PhotoManager.py
+++ backend/media/lib/PhotoManager.py
@@ -29,32 +29,44 @@ class PhotoService:
         for field, value in filters.items():
             if value:
                 if field == "camera__model":
-                    conditions.append(When(Q(camera__model=value), then=Value(4)))
+                    conditions.append(Case(When(Q(camera__model=value), then=Value(4)),
+                                                default=Value(0),
+                                                output_field=IntegerField()))
                 elif field == "lens__model":
-                    conditions.append(When(Q(lens__model=value), then=Value(4)))
+                    conditions.append(Case(Q(When(lens__model=value), then=Value(4)),
+                                                default=Value(0),
+                                                output_field=IntegerField()))
                 elif field == "ISO":
-                    conditions.append(When(Q(ISO__lte=int(value)+100) & Q(ISO__gte=int(value)-100), then=Value(3)))
+                    conditions.append(Case(When(Q(ISO__lte=int(value)+100) & Q(ISO__gte=int(value)-100), then=Value(3)),
+                                                default=Value(0),
+                                                output_field=IntegerField()))
                 elif field == "focal_length":
-                    conditions.append(When(Q(focal_length=value), then=Value(3)))
+                    conditions.append(Case(When(Q(focal_length=value), then=Value(3)),
+                                                default=Value(0),
+                                                output_field=IntegerField()))
                 elif field == "shutter_speed":
-                    conditions.append(When(Q(shutter_speed__lte=value+(value/2)) & Q(shutter_speed__gte=value-(value/2)), then=Value(2)))
+                    conditions.append(Case(When(Q(shutter_speed__lte=value+(value/2)) & Q(shutter_speed__gte=value-(value/2)), then=Value(2)),
+                                                default=Value(0),
+                                                output_field=IntegerField()))
                 else:
-                    conditions.append(When(Q(**{field: value}), then=Value(1)))
+                    conditions.append(Case(When(Q(**{field: value}), then=Value(1)),
+                                                default=Value(0),
+                                                output_field=IntegerField()))
 
         if search_point:
             qs = qs.annotate(distance=Distance("location", search_point)) # includes distance weight
-            conditions += [
+            conditions += [Case(
                 When(distance__lte=D(m=100), then=Value(10)),
                 When(distance__gt=D(m=100), distance__lte=D(m=1000), then=Value(8)),
                 When(distance__gt=D(m=1000), distance__lte=D(m=10000), then=Value(6)),
                 When(distance__gt=D(m=10000), distance__lte=D(m=100000), then=Value(3)),
                 When(distance__gt=D(m=100000), distance__lte=D(m=1000000), then=Value(1)),
-            ]
-            print(conditions)
+                default=Value(0),
+                output_field=IntegerField()
+            )]
 
         if conditions:
-            qs = qs.annotate(relevance=Sum(
-                Case(*conditions, output_field=IntegerField())
-            ))
+            print(conditions)
+            qs = qs.annotate(relevance=sum(conditions))
             return qs.order_by("-relevance")
         return qs
