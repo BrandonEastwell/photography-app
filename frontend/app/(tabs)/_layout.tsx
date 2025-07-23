2359ee4ed95e049ede885015d8cb91138371685b
BrandonEastwell <brandoneastwell@yahoo.com>
2025-07-23 23:11:16 +0100

    Layout changes

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -12,55 +12,58 @@ export default function TabLayout() {
     const { onUploadClick, showUploadScreen, setShowUploadScreen } = useUpload()
 
     return (
-        <View style={{ flex: 1, backgroundColor: '#181a1b', maxWidth: 800, width: "100%", alignSelf: "center", overflow: "hidden" }}>
-            <HeaderBar />
-            <Tabs
-                screenOptions={{
-                    tabBarActiveTintColor: '#ffffff',
-                    headerShown: false,
-                    tabBarShowLabel: false,
-                    tabBarStyle: {
-                        backgroundColor: '#181a1b',
-                        borderTopWidth: 1,
-                        borderColor: '#212121',
-                        zIndex: 100
-                    }
-                }}
-            >
-                <Tabs.Screen
-                    name="index"
-                    options={{
-                        tabBarIcon: () => (
-                            <Pressable style={{ justifyContent: 'center', alignItems: 'center', flex: 1, top: 5 }}>
-                                <Entypo name="home" size={24} color="white" />
-                            </Pressable>
-                        )
+        <View style={{ flex: 1, backgroundColor: '#181a1b', width: "100%", alignItems: "center" }}>
+            <View style={{ flex: 1, maxWidth: 800, width: "100%", overflow: "hidden" }}>
+                <HeaderBar />
+                <Tabs
+                    screenOptions={{
+                        tabBarActiveTintColor: '#ffffff',
+                        headerShown: false,
+                        tabBarShowLabel: false,
+                        tabBarStyle: {
+                            backgroundColor: '#181a1b',
+                            borderTopWidth: 1,
+                            borderColor: '#212121',
+                            zIndex: 100
+                        }
                     }}
-                />
-                <Tabs.Screen
-                    name="upload"
-                    options={{
-                        tabBarButton: () => (
-                            <Pressable onPress={onUploadClick} style={{ justifyContent: 'center', alignItems: 'center', flex: 1, padding: 5 }}>
-                                <Entypo name="circle-with-plus" size={24} color="#ffffff" />
-                            </Pressable>
-                        )
-                    }}
-                />
-                <Tabs.Screen
-                    name="profile"
-                    options={{
-                        tabBarIcon: () => (
-                            <Pressable style={{ justifyContent: 'center', alignItems: 'center', flex: 1, top: 5  }}>
-                                <Ionicons name={'person-circle-sharp'} color={'#ffffff'} size={24} />
-                            </Pressable>
-                        )
-                    }}
-                />
-                <Tabs.Screen name="auth/login" options={{ href: null }}></Tabs.Screen>
-                <Tabs.Screen name="auth/register" options={{ href: null }}></Tabs.Screen>
-            </Tabs>
-            { showUploadScreen && <PhotoUpload setShowUpload={setShowUploadScreen} /> }
+                >
+                    <Tabs.Screen
+                        name="index"
+                        options={{
+                            tabBarIcon: () => (
+                                <Pressable style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+                                    <Entypo name="home" size={24} color="white" />
+                                </Pressable>
+                            )
+                        }}
+                    />
+                    <Tabs.Screen
+                        name="upload"
+                        options={{
+                            tabBarButton: () => (
+                                <Pressable onPress={onUploadClick} style={{ justifyContent: 'center', alignItems: 'center', flex: 1, padding: 5 }}>
+                                    <Entypo name="circle-with-plus" size={24} color="#ffffff" />
+                                </Pressable>
+                            )
+                        }}
+                    />
+                    <Tabs.Screen
+                        name="profile"
+                        options={{
+                            tabBarIcon: () => (
+                                <Pressable style={{ justifyContent: 'center', alignItems: 'center', flex: 1  }}>
+                                    <Ionicons name={'person-circle-sharp'} color={'#ffffff'} size={24} />
+                                </Pressable>
+                            )
+                        }}
+                    />
+                    <Tabs.Screen name="auth/login" options={{ href: null }}></Tabs.Screen>
+                    <Tabs.Screen name="auth/register" options={{ href: null }}></Tabs.Screen>
+                </Tabs>
+                { showUploadScreen && <PhotoUpload setShowUpload={setShowUploadScreen} /> }
+            </View>
+
         </View>
     );
 }
