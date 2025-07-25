a4eaa70deb776b92625f7cf0d8b90b17d6c93a2a
BrandonEastwell <brandoneastwell@yahoo.com>
2025-07-26 00:45:05 +0100

    Animated sidebar button

--- frontend/app/components/HeaderBar.tsx
+++ frontend/app/components/HeaderBar.tsx
@@ -8,6 +8,7 @@ export default function HeaderBar({ containerWidth } : { containerWidth: number 
     const titleSlide = useRef(new Animated.Value(0)).current;
 
     const closeSidebar = () => {
+        toggleMenu()
         Animated.parallel([
             Animated.timing(sidebarWidth, {
                 toValue: 0,
@@ -21,11 +22,14 @@ export default function HeaderBar({ containerWidth } : { containerWidth: number 
                 easing: Easing.out(Easing.ease),
                 useNativeDriver: true,
             })
-        ]).start(() => setShowSidebar(false));
+        ]).start(() => {
+            setShowSidebar(false)
+        });
 
     };
 
     const openSidebar = () => {
+        toggleMenu()
         setShowSidebar(true)
         Animated.parallel([
             Animated.timing(sidebarWidth, {
@@ -42,6 +46,54 @@ export default function HeaderBar({ containerWidth } : { containerWidth: number 
             })
         ]).start();
     };
+    const animation = useRef(new Animated.Value(0)).current;
+
+    const toggleMenu = () => {
+        Animated.timing(animation, {
+            toValue: showSidebar ? 0 : 1,
+            duration: 150,
+            useNativeDriver: false,
+        }).start();
+    };
+
+    const topLineTransform = {
+        transform: [
+            {
+                rotate: animation.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: ['0deg', '45deg'],
+                }),
+            },
+            {
+                translateY: animation.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [0, 8.5], // adjust for bar spacing
+                }),
+            },
+        ],
+    };
+
+    const middleLineOpacity = animation.interpolate({
+        inputRange: [0, 1],
+        outputRange: [1, 0],
+    });
+
+    const bottomLineTransform = {
+        transform: [
+            {
+                rotate: animation.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: ['0deg', '-45deg'],
+                }),
+            },
+            {
+                translateY: animation.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [0, -8.5],
+                }),
+            },
+        ],
+    };
 
     return (
         <>
@@ -51,14 +103,33 @@ export default function HeaderBar({ containerWidth } : { containerWidth: number 
                         alignSelf: "center", marginHorizontal: "auto", transform: [{ translateX: titleSlide }] }}>Photography App</Animated.Text>
                 </View>
                 <Pressable onPress={() => showSidebar ? closeSidebar() : openSidebar()} style={{ alignSelf: "center",  padding: 15 }}>
-                    <MaterialIcons name="menu" size={24} color="white" />
+                    <Animated.View style={[{
+                        width: 20,
+                        height: 2,
+                        marginBottom: 4,
+                        backgroundColor: 'white'
+                    }, topLineTransform]} />
+                    <Animated.View style={[{
+                        width: 20,
+                        height: 2,
+                        marginBottom: 4,
+                        backgroundColor: 'white'
+                    }, { opacity: middleLineOpacity }]} />
+                    <Animated.View style={[{
+                        width: 20,
+                        height: 2,
+                        backgroundColor: 'white'
+                    }, bottomLineTransform]} />
                 </Pressable>
             </View>
             { showSidebar && <View style={{ position: "absolute", flexDirection: "row", width: "100%",
                 height: "100%", zIndex: 9 }}>
                 <Pressable onPress={() => closeSidebar()} style={{ flex: 1 }}></Pressable>
-                <Animated.View style={{ width: sidebarWidth, height: "100%", backgroundColor: "black" }}>
+                <Animated.View style={{ width: sidebarWidth, height: "100%", backgroundColor: '#121212',
+                    borderLeftWidth: 0.5, borderColor: "rgba(179,179,179,0.74)" }}>
+                    <View style={{ position: "absolute", width: "100%", height: "100%" }}>
 
+                    </View>
                 </Animated.View>
             </View> }
         </>
